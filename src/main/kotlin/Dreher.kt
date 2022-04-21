94ef02101e64def5d7bb67af48761d65aa1deefa
Phash <phash@phash.de>
2022-04-21 14:02:30 +0200

    format

--- src/main/kotlin/Dreher.kt
+++ src/main/kotlin/Dreher.kt
@@ -27,24 +27,24 @@ import java.io.File
 
 
 class Dreher {
-
     fun dreh(args: Array<String>) {
 
-        val file:File = File(args[0])
-       if (file.extension.equals(other = "pdf", ignoreCase = true)
-           || file.extension.equals(other = "pdfa", ignoreCase = true)) {
-           file.let {
-               pdfDrehen(it)
-           }
-       }
+        val file = File(args[0])
+        if (file.extension.equals(other = "pdf", ignoreCase = true)
+            || file.extension.equals(other = "pdfa", ignoreCase = true)
+        ) {
+            file.let {
+                pdfDrehen(it)
+            }
+        }
     }
 }
 
 fun pdfDrehen(file: File) {
     val doc = Loader.loadPDF(file)
-    var newDoc = PDDocument()
-    doc.documentCatalog.pages.reversed(). forEach{
-        it.rotation=180
+    val newDoc = PDDocument()
+    doc.documentCatalog.pages.reversed().forEach {
+        it.rotation = 180
         newDoc.addPage(it)
     }
     newDoc.save("${file.nameWithoutExtension}_gedreht.pdf")
